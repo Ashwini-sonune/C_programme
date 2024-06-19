7a0a6ec04397524be739a48a22ce87ce402b0f3d
Ashwini-sonune <128899642+Ashwini-sonune@users.noreply.github.com>
2024-06-19 23:41:29 +0530

    Add files via upload

---
+++ star_pattern2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("\t\t*");
+    printf("\n\t*\t\t*");
+    printf("\n*\t\t*\t\t*");
+    printf("\n\t*\t\t*");
+    printf("\n\t\t*");
+}
---
+++ star_pattern3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf("\t*\t*\t*\t*\t*");
+    printf("\n\n\t\t*\t*\t*\t*");
+    printf("\n\n\t\t\t*\t*\t*");
+    printf("\n\n\t\t\t\t*\t*");
+    printf("\n\n\t\t\t\t\t*");
+}
