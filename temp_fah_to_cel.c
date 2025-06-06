a63901f0481abc5c16fb3041591696b1b3d781b2
Ashwini-sonune <128899642+Ashwini-sonune@users.noreply.github.com>
2025-06-06 23:27:31 +0530

    Add files via upload

---
+++ temp_fah_to_cel.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+float main()
+{
+    float fah,cel;
+    printf("Enter tempreture in fahrhnehit");
+    scanf("%f",&fah);
+    cel=5.0/9.0*(fah-32);
+    printf("Temprature in celcius =%f",cel);
+}
